03a8e43e8104890dd3dbd0da00365681f16fccac
Rujul-Matta <latamatta1205@gmail.com>
2020-11-28 12:46:04 +0530

    Hogi

--- sketch.js
+++ sketch.js
@@ -9,18 +9,11 @@ function setup(){
 
     ball = createSprite(250,250,50,50);
     ball.shapeColor = "red";
-
-    ball_1 = createSprite(200,200,75,75);
-    ball_1.shapeColor = "blue"
-
-
-
-    
 }
 
 function draw(){
     background("white");
-    edges = createEdgeSprites();
+    
     if(position !== undefined){
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
@@ -34,25 +27,19 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
-    move_1(ball_1,1)
-    ball.bounceOff(edges)
-    // bounceOff(ball_1, edges)
+    
     drawSprites();
 }
 }
 
 function changePosition(x,y){
     data1.ref('ball/position').set({'x': position.x + x, 'y': position.y+y})
-    // data1.ref('ball_1/position').set({'x': position.x + x, 'y': position.y+y})
 }
 
 function readPosition(data){
     position = data.val();
     ball.x = position.x; 
     ball.y = position.y; 
-
-    // ball_1.x = postion.x;
-    // ball_1.y = position.y;
 }
 
 function showError() {
